0a5891828c97f1db221023d04880be17f7039960
lurisz33 <lukaspaul@bluewin.ch>
2026-03-26 09:43:55 +0100

    Implement user entity

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
@@ -29,10 +29,13 @@ public class User implements Serializable {
     private String username;
 
     @Column(nullable = false, unique = true)
+    private String password;
+
+    @Column(nullable = false, unique = true)
     private String token;
 
     @Column(nullable = false)
-    private UserStatus status;
+    private String createdAt;
 
     public Long getId() {
         return id;
@@ -50,6 +53,14 @@ public class User implements Serializable {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getToken() {
         return token;
     }
@@ -58,11 +69,11 @@ public class User implements Serializable {
         this.token = token;
     }
 
-    public UserStatus getStatus() {
-        return status;
+    public String getCreatedAt() {
+        return createdAt;
     }
 
-    public void setStatus(UserStatus status) {
-        this.status = status;
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
     }
 }
