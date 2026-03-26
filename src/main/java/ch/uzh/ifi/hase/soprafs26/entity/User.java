5561a50e325136f064586774c888b2bbf1164957
lurisz33 <lukaspaul@bluewin.ch>
2026-03-26 10:51:57 +0100

    More cleaning up

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
@@ -4,21 +4,9 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 
-/**
- * Internal User Representation
- * This class composes the internal representation of the user and defines how
- * the user is stored in the database.
- * Every variable will be mapped into a database field with the @Column
- * annotation
- * - nullable = false -> this cannot be left empty
- * - unique = true -> this value must be unqiue across the database -> composes
- * the primary key
- */
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue
